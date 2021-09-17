7da29d450b5b5b5eff4a64b4ccad66dc1ad50733
Ashwani Kumar Maurya <17217@iiitu.ac.in>
2021-09-17 22:44:04 +0530

    User Login Check

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -47,8 +47,41 @@ router.post('/createuser', [
     // res.json({error: 'Please enter a unique value for email', message: err.message})})
   } catch (error) {
       console.error(error.message)
-      res.status(500).send("Some Error Occured")
+      res.status(500).send("Internal Server Error")
   }
 })
 
+// Authentication: POST "/api/auth/login".
+router.post('/login', [
+  body('email','Enter a valid Email').isEmail(),
+  body('password','Password cannot be empty').exists(),
+], async (req, res)=>{
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const {email, password} = req.body
+  try {
+    let user = await User.findOne({email})
+    if (!user){
+      return res.status(400).json({error: "Please try to login with correct credentials"})
+    }
+    const passwordCompare = await bcrypt.compare(password, user.password)
+    if (!passwordCompare){
+      return res.status(400).json({error: "Please try to login with correct credentials"})
+    }
+    const data={
+      user:{
+        id: user.id
+      }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET)
+    console.log(authtoken);
+    res.json({authtoken})
+
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send("Internal Server Error")
+  }
+})
 module.exports = router
